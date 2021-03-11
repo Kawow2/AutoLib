cc56edf17ea62176b3be1290d919f062cf85c7fe
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2021-03-11 17:35:52 +0100

    Ajout de la connection à la BD

---
+++ AutoLib/AutoLib/Models/DAO/ServiceAutolib.cs
@@ -0,0 +1,30 @@
+﻿using AutoLib.Models.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoLib.Models.DAO
+{
+    public class ServiceAutolib
+    {
+        private static ServiceAutolib instance;
+        private static autolibContext dbContext;
+
+
+        public static ServiceAutolib getInstance()
+        {
+            if (ServiceAutolib.instance == null)
+            {
+                ServiceAutolib.instance = new ServiceAutolib();
+                dbContext = new autolibContext();
+            }
+
+            return ServiceAutolib.instance;
+        }
+        private ServiceAutolib()
+        {
+
+        }
+    }
+}
--- AutoLib/AutoLib/Startup.cs
+++ AutoLib/AutoLib/Startup.cs
@@ -4,10 +4,12 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoLib.Models.Domain;
 
 namespace AutoLib
 {
@@ -24,6 +26,8 @@ namespace AutoLib
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<autolibContext>(options =>
+                        options.UseMySql(Configuration.GetConnectionString("ConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
