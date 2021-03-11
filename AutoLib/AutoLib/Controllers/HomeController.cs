7d2fa62a5d1d4d683c55f3d6ea73d0239a4896f7
Antoine BIGEARD <antoine.bigeard.tpe@gmail.com>
2021-03-11 18:12:39 +0100

    Ajout de la vue de Connexion

--- AutoLib/AutoLib/Controllers/HomeController.cs
+++ AutoLib/AutoLib/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace AutoLib.Controllers
         {
             return View();
         }
+        public IActionResult Connexion()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
